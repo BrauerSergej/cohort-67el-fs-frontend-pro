665818c916996ee7eb0654944f313eadcd8e74a6
Brauer Sergej <bmw325d@web.de>
2026-02-08 13:09:13 +0100

    add homework 02

---
+++ React/src/homeworks/Homework_02/Homework_02.jsx
@@ -0,0 +1,17 @@
+import Card from "../../components/Card/Card";
+import "./styles.css";
+
+function Homework_02() {
+  return (
+    <div className="wrapper">
+      <h2 className="homework_title">Homework 02 - Profile Cards</h2>
+      <div className="cards_container">
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </div>
+  );
+}
+
+export default Homework_02;
