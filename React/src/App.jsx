c88c2ab71958f69ee0f44d97efba98031ea788b9
Brauer Sergej <bmw325d@web.de>
2026-02-09 20:08:37 +0100

    Save work on Lesson 04 before switching

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,6 +1,7 @@
 // Lessons
 // import Lesson_02 from "./lessons/Lesson_02/Lesson_02";
 // import Lesson_03 from "./lessons/Lesson_03/Lesson_03";
+import Lesson_04 from "./lessons/Lesson_04/Lesson_04";
 
 // Homeworks
 // import Homework_02 from "./homeworks/Homework_02/Homework_02";
@@ -12,8 +13,10 @@ function App() {
       {/* LESSONS */}
       {/* <Lesson_02 /> */}
       {/* <Lesson_03 /> */}
+      <Lesson_04 />
       {/* HOMEWORKS */}
       {/* <Homework_02 /> */}
+      {/* <Homework_03 /> */}
       {/* <Homework_03 /> */}
     </>
   );
--- React/src/lessons/Lesson_04/Lesson_04.jsx
+++ React/src/lessons/Lesson_04/Lesson_04.jsx
@@ -1,9 +1,6 @@
 import "./styles.css";
 
 function Lesson_04() {
-  return (
-    <div>
-      <h2>Lesson 04</h2>
-    </div>
-  );
+  return <div className="lesson_04_page_wrapper"></div>;
 }
+export default Lesson_04;
