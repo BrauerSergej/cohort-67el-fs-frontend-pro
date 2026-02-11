fd62d39f403fbc309f933cda46cf3f9c0eb9f21e
Brauer Sergej <bmw325d@web.de>
2026-02-11 16:39:58 +0100

    add homework 04

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,11 +1,15 @@
 // Lessons
 // import Lesson_02 from "./lessons/Lesson_02/Lesson_02";
 // import Lesson_03 from "./lessons/Lesson_03/Lesson_03";
-import Lesson_04 from "./lessons/Lesson_04/Lesson_04";
+// import Lesson_04 from "./lessons/Lesson_04/Lesson_04";
 
 // Homeworks
 // import Homework_02 from "./homeworks/Homework_02/Homework_02";
 // import Homework_03 from "./homeworks/Homework_03/Homework_03";
+import Homework_04 from "./homeworks/Homework_04/Homework_04";
+
+// My Projects
+// import Project_fb from "./my_projects/Project_fb";
 
 function App() {
   return (
@@ -13,11 +17,14 @@ function App() {
       {/* LESSONS */}
       {/* <Lesson_02 /> */}
       {/* <Lesson_03 /> */}
-      <Lesson_04 />
+      {/* <Lesson_04 /> */}
       {/* HOMEWORKS */}
       {/* <Homework_02 /> */}
       {/* <Homework_03 /> */}
       {/* <Homework_03 /> */}
+      <Homework_04 />
+      {/*My_Projects*/}
+      {/* <Project_fb/> */}
     </>
   );
 }
---
+++ React/src/homeworks/Homework_04/Homework_04.jsx
@@ -0,0 +1,12 @@
+import "./styles.css";
+import Feedback from "../../components/Feedback/Feedback";
+
+function Lesson_04() {
+  return (
+    <div className="wrapper_hw_04">
+      <Feedback />
+    </div>
+  );
+}
+
+export default Lesson_04;
